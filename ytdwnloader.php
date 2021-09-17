0ccfe8aad9905f8823b98ae306d99b3fb4171bac
ZxAgung <49530313+Z-zxQ@users.noreply.github.com>
2021-09-17 17:24:45 +0700

    Create ytdwnloader.php

---
+++ ytdwnloader.php
@@ -0,0 +1,227 @@
+
+<!DOCTYPE html>
+<html>
+<!--WH173 5P1D3R is Here --!>
+	<head>
+	<link rel="icon" href="https://i.ibb.co/KhGS611/74d243607c07.jpg">
+	<script data-ad-client="ca-pub-5942903955872320" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	</script> 
+	<style type="text/css">
+		body{
+			background:url(https://f.top4top.io/p_1577h5txv0.gif);		}
+		.shining {
+		font-size: 25px;
+		font-family: Gloria Hallelujah;
+		 text-align:center;
+		 text-transform:uppercase;
+		 letter-spacing:5px;
+		 background:linear-gradient(90deg, #000,#f5f5f5,#000);
+		 -webkit-background-clip:text;
+		 -webkit-text-fill-color:transparent;
+		 background-repeat:no-repeat;
+		 background-size:80%;
+		 position:relative;
+		 animation:shine 6s ease-in-out infinite;
+}
+.shining h1{
+		font-size: 50px;
+}
+@keyframes shine {
+from {
+background-position-x:-500%;
+}
+to {
+background-position-x:500%;
+}
+}
+p{
+	font-size: 15px;
+	font-family: Indie Flower;
+	color: white;
+}
+	</style>
+	<script type="text/javascript">
+		$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){
+		particlesJS('particles-js', {
+		"particles": {
+		"number": {
+		"value": 80,
+		"density": {
+		"enable": true,
+		"value_area": 800
+		} }, "color": {
+		"value": "#fff" },
+		"shape": { "type": "circle",
+		"stroke": { "width": 0, "color": "#000000" },
+		"polygon": { "nb_sides": 5 },
+		"image": { "width": 100, "height": 100 } },
+		"opacity": { "value": 0.5, "random": false,
+		"anim": {
+		"enable": false,
+		"speed": 1,
+		"opacity_min": 0.1,
+		"sync": false } },
+		"size": {
+		"value": 5,
+		"random": true,
+		"anim": {
+		"enable": false,
+		"speed": 40,
+		"size_min": 0.1,
+		"sync": false } },
+		"line_linked": {
+		"enable": true,
+		"distance": 150,
+		"color": "#FF0000",
+		"opacity": 0.4,
+		"width": 1 },
+		"move": {
+		"enable": true,
+		"speed": 2,
+		"direction": "none",
+		"random": false,
+		"straight": false,
+		"out_mode": "out",
+		"attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } },
+		"interactivity": {
+		"detect_on": "canvas",
+		"events": {
+		"onhover": {
+		"enable": true, "mode": "repulse" },
+		"onclick": {
+		"enable": true,
+		"mode": "push" },
+		"resize": true },
+		"modes": {
+		"grab": { "distance": 400, "line_linked": {
+		"opacity": 1 } },
+		"bubble": { "distance": 400, "size": 50, "duration": 2, "opacity": 8, "speed": 1 },
+		"repulse": { "distance": 200 },
+		"push": { "particles_nb": 4 }, "remove": {
+		"particles_nb": 2 } } },
+		"retina_detect": true, "config_demo": {
+		"hide_card": false,
+		"background_color": "#b61924",
+		"background_image": "",
+		"background_position": "50% 50%",
+		"background_repeat":
+		"no-repeat",
+		"background_size": "cover"
+		}
+		});
+		});
+		</script>
+	<style type="text/css">body {cursor:url(""),default}</style>
+<div align="center"><table border="0" width="100%"><tr><td>
+<font size="200">
+<b><font face="times"><b><center><SCRIPT>
+farbbibliothek = new Array();
+farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100");
+farbbibliothek[1] = new Array("#FF0000","#FFFFFF","#FFFFFF","#FF0000");
+farbbibliothek[2] = new Array("#FFFFFF","#FF0000","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF");
+farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040");
+farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000");
+farbbibliothek[5] = new Array("#FF0000","#FF0000","#FF0000","#FFFFFF","#FFFFFF","#FFFFFF");
+farbbibliothek[6] = new Array("#FF0000","#FDF5E6");
+farben = farbbibliothek[4];
+function farbschrift()
+{
+for(var i=0 ; i<Buchstabe.length; i++)
+{
+document.all["a"+i].style.color=farben[i];
+}
+farbverlauf();
+}
+function string2array(text)
+{
+Buchstabe = new Array();
+while(farben.length<text.length)
+{
+farben = farben.concat(farben);
+}
+k=0;
+while(k<=text.length)
+{
+Buchstabe[k] = text.charAt(k);
+k++;
+}
+}
+function divserzeugen()
+{
+for(var i=0 ; i<Buchstabe.length; i++)
+{
+document.write("<span id='a"+i+"' class='a"+i+"'>"+Buchstabe[i] + "</span>");
+}
+farbschrift();
+}
+var a=1;
+function farbverlauf()
+{
+for(var i=0 ; i<farben.length; i++)
+{
+farben[i-1]=farben[i];
+}
+farben[farben.length-1]=farben[-1];
+
+setTimeout("farbschrift()",30);
+}
+//
+var farbsatz=1;
+function farbtauscher()
+{
+farben = farbbibliothek[farbsatz];
+while(farben.length<text.length)
+{
+farben = farben.concat(farben);
+}
+farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001));
+}
+setInterval("farbtauscher()",5000);
+text ='YOUTUBE DOWNLOADER';//h
+string2array(text);
+divserzeugen();
+//document.write(text);
+</script>
+</head>
+<body>
+	<center>
+	<div class="shining">
+		<font color="red" size="2">by WH173 5P1D3R</font>
+	</div>
+<div class="row">
+<div class="col-md-12 grid-margin stretch-card">
+<div class="card">
+<div class="card-body">
+<center>
+  <iframe src="https://mp3downy.com/MP3-converter?apikey=1234567890&color=17a2b8&bg=000000" style="width:100%;min-height:350px;height:auto;border:0px;" ></iframe>  
+</center>
+<div id="coba"></div>
+</center>
+<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
+				<p class="text-muted text-center text-md-left">
+					Copyright © 2021 <a href="http://wh173-5p1d3r-scripts1.rf.gd/" target="_blank">WH173 5P1D3R</a> 
+				</p>
+				<p class="text-muted text-center text-md-left mb-0 d-none d-md-block">
+					Coded <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i> by WH173 5P1D3R
+				</p>
+			</footer>
+		</div>
+	</div>
+
+	<script src="files/core.js"></script>
+	<script src="files/hmm.min.js"></script>
+	
+	<script src="files/dinasayang.min.js"></script>
+	
+	<script src="files/akusayangdina.min.js"></script>
+	<script src="files/dina.js"></script>
+	<script src="files/ilovedina.js"></script>
+	<script src="files/dinasayangku.js"></script>
+<iframe allow="autoplay" allowfullscreen="0" frameborder="0" height="0" loop='true' src='https://f.top4top.io/m_1930rpnsi1.mp3' width='0'>
+    </iframe>	
+	</body>
+</html>
+	
